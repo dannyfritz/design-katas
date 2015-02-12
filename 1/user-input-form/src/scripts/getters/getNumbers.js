6915215b8d453f478ee1622f9fef191327a62761
Danny Fritz <dannyfritz@gmail.com>
2015-02-11 18:02:06 -0600

    remove unused files.

--- 1/user-input-form/src/scripts/getters/getNumbers.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-const Getter = require('nuclear-js').Getter;
-
-module.exports = Getter('numbers', numbers => numbers.toJS());
--- 1/user-input-form/src/scripts/reactors/dataReactor.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const Reactor = require('nuclear-js').Reactor;
-const numbersStore = require('../stores/numbersStore.js');
-
-module.exports = Reactor({
-	stores: {
-		numbers: numbersStore
-	}
-});
